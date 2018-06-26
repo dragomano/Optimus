dfe8e776a63234cac1e9b1d10b8e640dd9feb9d5
Bugo <bugo@dragomano.ru>
2018-06-26 22:17:20 +0500

    Update for SMF 2.1 Beta 4

---
+++ settings.php
@@ -0,0 +1,33 @@
+<?php
+
+if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+	require_once(dirname(__FILE__) . '/SSI.php');
+elseif(!defined('SMF'))
+	die('<b>Error:</b> Cannot install - please verify that you put this file in the same place as SMF\'s index.php and SSI.php files.');
+
+if ((SMF == 'SSI') && !$user_info['is_admin'])
+	die('Admin privileges required.');
+
+// Scheduled Tasks
+$rows = array(
+	array(
+		'method' => 'ignore',
+		'table_name' => '{db_prefix}scheduled_tasks',
+		'columns' => array(
+			'next_time'       => 'int',
+			'time_offset'     => 'int',
+			'time_regularity' => 'int',
+			'time_unit'       => 'string',
+			'disabled'        => 'int',
+			'task'            => 'string'
+		),
+		'data' => array(0, 0, 1, 'd', 1, 'optimus_sitemap'),
+		'keys' => array('id_task')
+	)
+);
+
+foreach ($rows as $row)
+	$smcFunc['db_insert']($row['method'], $row['table_name'], $row['columns'], $row['data'], $row['keys']);
+
+if (SMF == 'SSI')
+	echo 'Database changes are complete! Please wait...';
