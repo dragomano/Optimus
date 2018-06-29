8df8ef6061a8d13be5b2981f8b1f89f118c3223e
Bugo <bugo@dragomano.ru>
2018-06-29 07:17:44 +0500

    Update mod link

--- sources/subs/Optimus.class.php
+++ sources/subs/Optimus.class.php
@@ -4,7 +4,7 @@
  * Optimus.class.php
  *
  * @package Optimus
- * @link https://custom.simplemachines.org/mods/index.php?mod=2659
+ * @link https://addons.elkarte.net/feature/Optimus.html
  * @author Bugo https://dragomano.ru/mods/optimus
  * @copyright 2010-2018 Bugo
  * @license https://opensource.org/licenses/artistic-license-2.0 Artistic-2.0
