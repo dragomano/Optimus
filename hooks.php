2d9d2eaf2458ecd44bbd3779562552f1de7ab3c1
Bugo <bugo@dragomano.ru>
2020-05-30 08:26:53 +0500

    Add version checking on install

--- hooks.php
+++ hooks.php
@@ -5,6 +5,9 @@ if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
 elseif(!defined('SMF'))
 	die('<b>Error:</b> Cannot install - please verify that you put this file in the same place as SMF\'s index.php and SSI.php files.');
 
+if (version_compare(PHP_VERSION, '5.6', '<'))
+	die('This mod needs PHP 5.6 or greater. You will not be able to install/use this mod, contact your host and ask for a php upgrade.');
+
 if ((SMF == 'SSI') && !$user_info['is_admin'])
 	die('Admin privileges required.');
 
