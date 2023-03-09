4f5f583ff32af6eb28af5b9eeb2d760c283ac660
Bugo <cheetah@bk.ru>
2023-03-09 09:01:39 +0500

    Update copyrights

--- Sources/Optimus/Keywords.php
+++ Sources/Optimus/Keywords.php
@@ -10,7 +10,7 @@ namespace Bugo\Optimus;
  * @package Optimus
  * @link https://custom.simplemachines.org/mods/index.php?mod=2659
  * @author Bugo https://dragomano.ru/mods/optimus
- * @copyright 2010-2022 Bugo
+ * @copyright 2010-2023 Bugo
  * @license https://opensource.org/licenses/artistic-license-2.0 Artistic-2.0
  *
  * @version 2.10
