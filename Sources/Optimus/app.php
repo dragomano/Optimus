b36e227d57a2626c890bcb533270f22f984e656f
Bugo <cheetah@bk.ru>
2023-03-15 11:23:22 +0500

    Update version to 2.11

--- Sources/Optimus/app.php
+++ Sources/Optimus/app.php
@@ -4,7 +4,7 @@ if (! defined('SMF'))
 	die('No direct access...');
 
 defined('OP_NAME') || define('OP_NAME', 'Optimus for SMF');
-defined('OP_VERSION') || define('OP_VERSION', '2.10.6');
+defined('OP_VERSION') || define('OP_VERSION', '2.11');
 
 function optimus_autoloader($classname)
 {
