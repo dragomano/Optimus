b7fa29a42cf569ea77f5ad1fdc4634d6c47f5875
Bugo <cheetah@bk.ru>
2024-02-05 01:04:56 +0500

    Move boostrap.php into Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,8 @@
 <?php declare(strict_types=1);
 
+use Bugo\Compat\Config;
+use Bugo\Compat\Theme;
+use Bugo\Compat\User;
 use Bugo\Compat\Utils;
 
 /*
@@ -31,7 +34,22 @@ use Bugo\Compat\Utils;
 });*/
 uses()->beforeAll(function () {
 	require_once dirname(__DIR__) . '/src/Sources/Optimus/app.php';
-	require_once __DIR__ . '/boostrap.php';
+
+	User::$info['language'] = 'english';
+
+	Config::$sourcedir = __DIR__ . '/files';
+
+	Theme::$current->settings['default_images_url'] = '';
+
+	Utils::$smcFunc['substr'] = fn($string, $offset, $length) => substr($string, $offset, $length);
+	Utils::$smcFunc['strlen'] = fn($string) => strlen($string);
+	Utils::$smcFunc['htmltrim'] = fn($string) => trim($string);
+	Utils::$smcFunc['htmlspecialchars'] = fn($string, $flags) => htmlspecialchars($string, $flags);
+	Utils::$smcFunc['db_query'] = fn(...$params) => ['foo' => 'bar'];
+	Utils::$smcFunc['db_fetch_assoc'] = fn($result) => ['foo' => 'bar'];
+	Utils::$smcFunc['db_fetch_row'] = fn($result) => ['bar'];
+	Utils::$smcFunc['db_free_result'] = fn($result) => true;
+	Utils::$smcFunc['db_insert'] = fn(...$params) => true;
 
 	loadLanguage('Optimus/Optimus');
 })->in(__DIR__);
