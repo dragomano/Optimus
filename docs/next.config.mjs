124e986bb178326be96b68ef1568f7f18ed62d96
Bugo <cheetah@bk.ru>
2024-12-11 17:46:12 +0500

    Update next.config.mjs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,13 +1,15 @@
-import nextra from 'nextra'
+import nextra from 'nextra';
 
 const withNextra = nextra({
+  output: 'export',
+  //basePath: '/Optimus',
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx'
-})
+  themeConfig: './theme.config.jsx',
+});
 
 export default withNextra({
   i18n: {
     locales: ['en', 'ru'],
-    defaultLocale: 'en'
-  }
-})
+    defaultLocale: 'en',
+  },
+});
