b4ff1209f3ff1671367b51621c6ce5b1bd523a44
Bugo <cheetah@bk.ru>
2025-01-01 12:03:48 +0500

    Update version

--- src/Sources/Optimus/Addons/SafeLinks.php
+++ src/Sources/Optimus/Addons/SafeLinks.php
@@ -4,7 +4,7 @@
  * @package SafeLinks (Optimus)
  * @link https://custom.simplemachines.org/mods/index.php?mod=2659
  * @author Bugo https://dragomano.ru/mods/optimus
- * @copyright 2010-2024 Bugo
+ * @copyright 2010-2025 Bugo
  * @license https://opensource.org/licenses/artistic-license-2.0 Artistic-2.0
  *
  * @category addon
--- src/Sources/Optimus/Services/FileSystemException.php
+++ src/Sources/Optimus/Services/FileSystemException.php
@@ -4,10 +4,10 @@
  * @package Optimus
  * @link https://custom.simplemachines.org/mods/index.php?mod=2659
  * @author Bugo https://dragomano.ru/mods/optimus
- * @copyright 2010-2024 Bugo
+ * @copyright 2010-2025 Bugo
  * @license https://opensource.org/licenses/artistic-license-2.0 Artistic-2.0
  *
- * @version 3.0 RC1
+ * @version 3.0 RC2
  */
 
 namespace Bugo\Optimus\Services;
